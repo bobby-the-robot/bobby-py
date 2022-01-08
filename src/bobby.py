de4794e49bcb115a4bc6ff951353aef616b4f167
Anton Hlinisty <anton.hlinisty@gmail.com>
2022-01-08 16:47:52 +0300

    Use Motor

--- src/bobby.py
+++ src/bobby.py
@@ -1,5 +1,7 @@
-from gpiozero import Robot
+from gpiozero import Motor
 
+#bobby = Robot(left=(3,4), right=(25,26))
+#bobby.forward()
 
-bobby = Robot(left=(3,4), right=(25,26))
-bobby.forward()
+right = Motor(25, 26)
+right.forward()
