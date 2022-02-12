51eed9fb8b753b63e691493a9cf2ed3968834f77
Anton Hlinisty <anton.hlinisty@gmail.com>
2022-02-12 19:21:50 +0300

    Fix threading

--- src/amqp_connection.py
+++ src/amqp_connection.py
@@ -6,10 +6,9 @@ from config import Config
 
 class ChannelFactory:
     def __init__(self):
-        credentials = PlainCredentials(Config.rabbit_user, Config.rabbit_password)
-        connection = BlockingConnection(
-            ConnectionParameters(Config.rabbit_host, Config.rabbit_port, Config.rabbit_user, credentials))
-        self.channel = connection.channel()
+        self.credentials = PlainCredentials(Config.rabbit_user, Config.rabbit_password)
 
     def get_channel(self):
-        return self.channel
+        connection = BlockingConnection(
+            ConnectionParameters(Config.rabbit_host, Config.rabbit_port, Config.rabbit_user, self.credentials))
+        return connection.channel()
--- src/bobby.py
+++ src/bobby.py
@@ -7,8 +7,10 @@ from amqp_connection import ChannelFactory
 amqp_channel_factory = ChannelFactory()
 ampq_channel = amqp_channel_factory.get_channel()
 
-ImageSender(ampq_channel)
 MessageReceiver(ampq_channel, Motion())
+ImageSender(ampq_channel) #check if it blocks anything
+
+print("Bingo!")
 
 #run forever
 loop = asyncio.get_event_loop()
