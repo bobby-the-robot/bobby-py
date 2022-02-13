bcbd3e0e0b4a9f39c0eee59bc829096e12e33877
Anton Hlinisty <anton.hlinisty@gmail.com>
2022-02-13 20:42:28 +0300

    Comment the app

--- src/bobby.py
+++ src/bobby.py
@@ -8,26 +8,27 @@ from amqp_connection import ChannelFactory
 amqp_channel_factory = ChannelFactory()
 
 
-def start_message_receiver():
-    MessageReceiver(amqp_channel_factory.get_channel(), Motion())
+#def start_message_receiver():
+#    MessageReceiver(amqp_channel_factory.get_channel(), Motion())
+
+MessageReceiver(amqp_channel_factory.get_channel(), Motion())
+
+#def start_image_sender():
+#    ImageSender(amqp_channel_factory.get_channel())
 
 
-def start_image_sender():
-    ImageSender(amqp_channel_factory.get_channel())
+#msg_thread = threading.Thread(target=start_message_receiver)
+#streaming_thread = threading.Thread(target=start_image_sender)
 
+#msg_thread.start()
+#streaming_thread.start()
 
-msg_thread = threading.Thread(target=start_message_receiver)
-streaming_thread = threading.Thread(target=start_image_sender)
-
-msg_thread.start()
-streaming_thread.start()
-
-msg_thread.join()
-streaming_thread.join()
+#msg_thread.join()
+#streaming_thread.join()
 
 #run forever
-loop = asyncio.get_event_loop()
-try:
-    loop.run_forever()
-finally:
-    loop.close()
+#loop = asyncio.get_event_loop()
+#try:
+#    loop.run_forever()
+#finally:
+#    loop.close()
