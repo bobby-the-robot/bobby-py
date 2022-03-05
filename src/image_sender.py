9f6398c96359de04338cc83f0841e8f0c06863a4
Anton Hlinisty <anton.hlinisty@gmail.com>
2022-03-05 16:33:27 +0300

    Fix queue name

--- src/image_sender.py
+++ src/image_sender.py
@@ -45,9 +45,9 @@ class ImageSender:
             while True:
                 with output.condition:
                     output.condition.wait()
-                    #self.ws.send_binary(output.frame)
                     print("123")
-                    self.ws.send("123")
+                    self.ws.send_binary(output.frame)
+                    #self.ws.send("123")
         finally:
             self.ws.close()
             self.camera.stop_recording()
