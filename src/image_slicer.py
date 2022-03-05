919cb4dc4bfb38fbd76c211346f0b38acfbe763a
Anton Hlinisty <anton.hlinisty@gmail.com>
2022-03-05 22:16:16 +0300

    Add stomp

--- src/image_slicer.py
+++ src/image_slicer.py
@@ -4,8 +4,7 @@ from threading import Condition
 from threading import Thread
 from config import Config
 from websocket import create_connection
-from stomp import Connection
-from stomp import PrintingListener
+import stomper
 import base64
 #from image_sender import ImageSender
 
@@ -41,23 +40,17 @@ class ImageSender:
         self.camera.rotation = 180
         self.camera.start_recording(output, format='mjpeg')
         try:
-            c = Connection([(Config.streaming_connection_url, 80)])
-            c.set_listener('', PrintingListener())
-            # self.c.connect('admin', 'password', wait=True)
-            c.connect(wait=True)
-            c.subscribe('/client', "123")
-            #self.ws = create_connection(Config.streaming_connection_url)
-            #self.ws.send("CONNECT\naccept-version:1.0,1.1,2.0\n\n\x00\n")
-            #sub = stomper.subscribe("/client", "MyuniqueId", ack="auto")
-            #self.ws.send(sub)
-            #self.ws.send(stomper.send("/client", "Hello there1"))
-            #self.ws.send(stomper.send("/client", "Hello there2"))
-            #self.ws.send(stomper.send("/client", "Hello there3"))
+            self.ws = create_connection(Config.streaming_connection_url)
+            self.ws.send("CONNECT\naccept-version:1.0,1.1,2.0\n\n\x00\n")
+            sub = stomper.subscribe("/client", "MyuniqueId", ack="auto")
+            self.ws.send(stomper.subscribe("/client", "MyuniqueId", ack="auto"))
+            self.ws.send(stomper.send("/client", "Hello there1"))
+            self.ws.send(stomper.send("/client", "Hello there2"))
+            self.ws.send(stomper.send("/client", "Hello there3"))
             while True:
                 with output.condition:
                     output.condition.wait()
                     #print(output.frame)
-                    c.send('/client', "123333")
                     #payload = 'aaa'
                     #try:
                     #    base64_data = base64.b64encode(output.frame)
@@ -68,8 +61,8 @@ class ImageSender:
                     #except Exception as e:
                     #    print("ERROR!!!!")
                     #    print(e)
-                    #sender.send("hello")
+                    self.ws.send(stomper.send("/client", output.frame))
         finally:
-            #self.ws.close()
+            self.ws.close()
             self.camera.stop_recording()
             self.camera.close()
