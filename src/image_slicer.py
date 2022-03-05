782b992b0b25486f7b7889dfc7aa2e878a24fc17
Anton Hlinisty <anton.hlinisty@gmail.com>
2022-03-05 21:10:34 +0300

    Add stomp

--- src/image_slicer.py
+++ src/image_slicer.py
@@ -63,5 +63,5 @@ class ImageSender:
                     #    print(e)
                     sender.send("hello")
         finally:
-            self.ws.close()
+            #self.ws.close()
             self.camera.stop_recording()
