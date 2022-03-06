aa8e4e9facb56524c58b4340e49df4a6d88fec67
Anton Hlinisty <anton.hlinisty@gmail.com>
2022-03-06 19:12:21 +0300

    Tackle camera params

--- src/image_slicer.py
+++ src/image_slicer.py
@@ -30,8 +30,8 @@ class StreamingOutput(object):
 class ImageSender:
     def __init__(self):
         print("Initializing video streaming...")
-        #self.camera = PiCamera(resolution='640x480', framerate=10)
-        self.camera = PiCamera(resolution='120x80', framerate=24)
+        self.camera = PiCamera(resolution='640x480', framerate=10)
+        #self.camera = PiCamera(resolution='120x80', framerate=24)
         thread1 = Thread(target=self.run)
         thread1.start()
 
