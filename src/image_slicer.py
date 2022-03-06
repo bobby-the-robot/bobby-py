1e7bb6c008615b14a707baa65344e54927623419
Anton Hlinisty <anton.hlinisty@gmail.com>
2022-03-06 18:10:26 +0300

    Send bytes

--- src/image_slicer.py
+++ src/image_slicer.py
@@ -48,9 +48,10 @@ class ImageSender:
             while True:
                 with output.condition:
                     output.condition.wait()
-                    msg = base64.b64encode(output.frame).decode('ascii')
-                    if msg:
-                        ws.send(stomper.send("/client", msg))
+                    #msg = base64.b64encode(output.frame).decode('ascii')
+                    #if msg:
+                    #    ws.send(stomper.send("/client", msg))
+                    ws.send(stomper.send("/client", output.frame))
         finally:
             self.camera.stop_recording()
             self.camera.close()
