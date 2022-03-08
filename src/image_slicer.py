0ee29f7655fddfe0c118f17d242ad10000e191ef
Anton Hlinisty <anton.hlinisty@gmail.com>
2022-03-08 21:09:28 +0300

    Rename stomp endpoint from client to video

--- src/image_slicer.py
+++ src/image_slicer.py
@@ -47,7 +47,7 @@ class ImageSender:
                 with output.condition:
                     output.condition.wait()
                     msg = base64.b64encode(output.frame).decode('ascii')
-                    ws.send(stomper.send("/client", msg))
+                    ws.send(stomper.send("/video", msg))
         finally:
             self.camera.stop_recording()
             self.camera.close()
