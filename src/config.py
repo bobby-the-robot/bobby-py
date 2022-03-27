05a170e6c8ee2e006c57570284a669c9a208dfc4
Anton Hlinisty <anton.hlinisty@gmail.com>
2022-03-27 14:04:49 +0300

    Fix motion topic name

--- src/config.py
+++ src/config.py
@@ -5,4 +5,4 @@ class Config:
     video_streaming_connection_url = os.getenv("VIDEO_STREAMING_CONNECTION_URL")
     video_streaming_destination = "/video"
     motion_control_connection_url = os.getenv("MOTION_CONTROL_CONNECTION_URL")
-    motion_control_topic = "/motion"
+    motion_control_topic = "/topic/motion"
