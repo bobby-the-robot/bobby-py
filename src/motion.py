3494d942ecc32e3683c6192724585e8fa9be9359
Anton Hlinisty <anton.hlinisty@gmail.com>
2022-09-18 21:35:21 +0200

    Try out timer instead of sleep

--- src/motion.py
+++ src/motion.py
@@ -14,19 +14,15 @@ class Motion:
         self.left = Motor(left_forward_pin, left_backward_pin)
 
     def stop(self):
-        print("stopping, unlocking")
         self.stop_motion()
         self.is_locked = False
 
     def move(self, direction):
-        self.move_forward()
         if self.is_locked:
-            print("can't move, locked")
             return
         print("Direction [%r] received" % direction)
         self.is_locked = True
         if direction == "FORWARD":
-            print("moving fwd")
             self.move_forward()
         elif direction == "RIGHT":
             self.turn_right()
